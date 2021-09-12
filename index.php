10e835fd9e3084e9d8369f1a8030f1c64af37c28
97PHPDeveloper <am.asky97@gmail.com>
2021-09-12 23:29:20 +0530

    hello world

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boostrab Web Page</title>
+
+    <!--Boostrab Css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <!--Css Link-->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="container">
+    
+        <div class="container">
+        <div class="row text-center ">
+            <div class="col-md-2 bg bg-primary ">
+                <img src="img.png" alt="" widt="130px" height="130px">
+            </div>
+            <div class="col-md-8 bg bg-primary text-white">
+                <div><h1>Hardy College of Technology</h1></div>    
+                <div><img src="Sri-Lanka_240-animated-flag-gifs.gif" alt="" width="80px" height="80px" class="img"></div>       
+            </div>
+            <div class="col-md-2 bg bg-primary">
+                <img src="images 1.png" alt="" widt="130px" height="130px">
+            </div>
+        </div>
+        </div>
+    
+
+    <div class="container main">
+        <div class="row">
+            <div class="col ini">
+            <marquee>online student registration</marquee>
+            </div>
+        </div>
+
+        <div class="row ">
+            <div class="col-md-3 ma h-60">
+                <a href="https://drive.google.com/u/0/uc?id=1kgrVk3j6FqHfMzxmGxq342OnIKLleaNv&export=download" download>download</a>
+            </div>
+            <div class="col-md-9 ma">
+                <div class="row">
+                    <div class="col-md-6 d-flex justify-content-center">
+                    <div class="card" style="width: 32rem;">
+                            <div class="card-header">
+                                Student 
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    06
+                                </li>
+                                <li class="list-group-item">A second item</li>
+                            </ul>
+                    </div>
+                    </div>
+
+                    
+                    <div class="col-md-6 d-flex justify-content-center">
+                    <div class="card" style="width: 32rem;">
+                            <div class="card-header">
+                                Student 
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">An item</li>
+                                <li class="list-group-item">A second item</li>
+                            </ul>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row text-center">
+            <div class="col-md bg bg-primary">fooder</div>
+        </div>
+    </div>
+</div>
+
+
+    <!--Boostrab Js-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
