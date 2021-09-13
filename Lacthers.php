3347599af24dab498a5dc1e97ce78828469f8989
97PHPDeveloper <am.asky97@gmail.com>
2021-09-13 14:32:06 +0530

    ajax change

--- Lacthers.php
+++ Lacthers.php
@@ -16,10 +16,10 @@
   ?>
 
   <div class="row">
-    <div class="col-2"><button onclick="loadfiles('inse','workArea')">Click Me</button></div>
+    <div class="col-md-2"><button onclick="loadfiles('inse','workArea')">Click Me</button></div>
 
 
-    <div class="col-10" id="workArea">Work Area</div>
+    <div class="col-md-10" id="workArea">Work Area</div>
   </div>
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
