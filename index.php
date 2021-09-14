9fc6db55edc7a5ab7d7e8e57fdfd935d5919f972
97PHPDeveloper <am.asky97@gmail.com>
2021-09-14 12:08:17 +0530

    marging

--- index.php
+++ index.php
@@ -32,14 +32,14 @@ include 'ajax.php';
             </div>
             
             <div class="col-md-2 bg bg-primary">
-                <nav class="navbar navbar-light fla" >
-                    <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-tagert="mynav">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </nav>
-                <img src="images 1.png" alt="" widt="120px" height="120px" class="img">
-                
+            <nav class="navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+                <img src="images 1.png" alt="" widt="120px" height="120px" class="img">   
             </div>
+</nav>
         </div>
         </div>
         
@@ -55,7 +55,8 @@ include 'ajax.php';
 
 <div class="container-fulid">
     <div class="row">
-        <div class="col-md-2 hei bg-dark">
+        <div class="col-md-2  bg-dark d-md-block collapse" id="navbarSupportedContent">
+            <div>
             <div>
                 <button class="btn"></button>
             </div>
@@ -82,6 +83,7 @@ include 'ajax.php';
 
             <div>
                 <button class="btn"><i class="fas fa-book-reader"></i><span>Pass Pepper</span></button>
+            </div>
             </div>
         </div>
 
@@ -142,6 +144,8 @@ include 'ajax.php';
 
 
     <!--Boostrab Js-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
